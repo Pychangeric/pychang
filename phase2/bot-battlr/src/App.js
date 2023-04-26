efd4a719d20f37157305b8619f7cb2c11fc1185d
pychangeric <pychangeric@gmail.com>
2023-04-26 23:19:44 +0300

    first part

--- phase2/bot-battlr/src/App.js
+++ phase2/bot-battlr/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from 'react';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <h1>Bot Battlr</h1>
+      <h2>Bot Collection</h2>
+      <BotCollection />
+      <h2>Your Bot Army</h2>
+      <YourBotArmy />
     </div>
   );
 }
---
+++ phase2/bot-battlr/src/components/BotCollection.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './BotCollection.css';
+
+const BotCollection = ({ enlistBot }) => {
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:3000/bots');
+      setBots(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="bot-collection">
+      {bots.map((bot) => (
+        <div key={bot.id} className="bot-card">
+          <div className="bot-card__image-container">
+            <img src={bot.image_url} alt={bot.name} />
+          </div>
+          <div className="bot-card__details">
+            <h2>{bot.name}</h2>
+            <p>Weapon: {bot.weapon}</p>
+            <p>Special Ability: {bot.special_ability}</p>
+            <button
+              onClick={() => enlistBot(bot)}
+              disabled={bot.enlisted}
+            >
+              {bot.enlisted ? 'Enlisted' : 'Enlist'}
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BotCollection;
